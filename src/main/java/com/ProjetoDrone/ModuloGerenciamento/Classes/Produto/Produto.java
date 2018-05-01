5153d29e5615a4f2505872a94f391e6c445a9439
Nakamura-PC <Nakamura-PC@DESKTOP-O3CVL0Q>
2018-05-01 13:22:09 -0300

    Ajustes finais do projeto e correções de erros.

--- src/main/java/com/ProjetoDrone/ModuloGerenciamento/Classes/Produto/Produto.java
+++ src/main/java/com/ProjetoDrone/ModuloGerenciamento/Classes/Produto/Produto.java
@@ -6,8 +6,6 @@
 package com.ProjetoDrone.ModuloGerenciamento.Classes.Produto;
 
 import java.io.Serializable;
-import java.util.Set;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -16,7 +14,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
