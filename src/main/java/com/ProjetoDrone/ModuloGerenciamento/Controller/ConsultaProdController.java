3cb18de7db6722607156d93151daee54dd02492b
Nakamura-PC <Nakamura-PC@DESKTOP-O3CVL0Q>
2018-05-25 16:25:01 -0300

    Merge origin/master
    Conflicts:
    	src/main/resources/templates/cadastrarProd.html

---
+++ src/main/java/com/ProjetoDrone/ModuloGerenciamento/Controller/ConsultaProdController.java
@@ -0,0 +1,31 @@
+package com.ProjetoDrone.ModuloGerenciamento.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nakamura-PC
+ */
+@Controller
+@RequestMapping("/consultar/produto")
+public class ConsultaProdController {
+    
+    
+//    
+//    @GetMapping
+//    public ModelAndView consulta(@PathVariable("nome") String nomeProd){
+//        
+//       
+//    }
+}
